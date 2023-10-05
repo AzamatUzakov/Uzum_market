7a56b563acfa67487adb3b8944f190af1eb1eca6
AzamatUzakov <azamatakmal053@gmail.com>
2023-10-05 20:25:05 +0500

    foooters js

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,30 +1,99 @@
 
 
-let arrow = document.querySelector('.fof')
-let def = document.querySelector('.defoult')
-console.log(arrow);
 
 
+function one_var() {
 
-    arrow.onclick = () => {
-        def.style.display = "block"
-        def.style.height = 100 + "px"
+    let one_variant = document.createElement('div')
 
-        arrow.style.rotate = 180 + 'deg'
 
-        if (arrow.style.rotate = 180 + 'deg') {
+    let about_us_contener = document.createElement('div')
+    let about_us_contener_h1 = document.createElement('h1')
+    let about_us_contener_a_1 = document.createElement('a')
+    let about_us_contener_a_2 = document.createElement('a')
 
-            arrow.onclick = () => {
-                def.style.display = "none"
-                def.style.height = 0 + "px"
-                arrow.style.rotate = 360 + 'deg'
-                console.log('lf,vl');
-            }
-        
-        
-        }
-    }
+    let for_users_contner = document.createElement('div')
+    let for_users_contner_h1 = document.createElement('h1')
+    let for_users_contner_a_1 = document.createElement('a')
+    let for_users_contner_a_2 = document.createElement('a')
+
+    let for_entrepreneurs_contner = document.createElement('div')
+    let for_entrepreneurs_contner_h1 = document.createElement('h1')
+    let for_entrepreneurs_contner_a_1 = document.createElement('a')
+    let for_entrepreneurs_contner_a_2 = document.createElement('a')
+
+
+    let application_box = document.createElement('div')
+    let flex_width_1200px = document.createElement('div')
+    let flex_width_1200px_h1 = document.createElement('h1')
+    let apllication_flex_box = document.createElement('div')
+
+    let apple_item = document.createElement('div')
+    let apple_item_img = document.createElement('img')
+    let apple_item_p = document.createElement('p')
+
+    let google_play_item = document.createElement('div')
+    let google_play_item_img = document.createElement('img')
+    let google_play_item_p = document.createElement('p')
+
+    let uzum_networks_cont = document.createElement('div')
+    let uzum_networks_cont_h1 = document.createElement('h1')
+    let networks = document.createElement('div')
+    let networks_img_INST = document.createElement('img')
+    let networks_img_TG = document.createElement('img')
+    let networks_img_YOU = document.createElement('img')
+    let networks_img_FC = document.createElement('img')
+
+    let down = document.createElement('div')
+    let down_hr = document.createElement('hr')
+    let down_flex = document.createElement('div')
+
+    let down_title = document.createElement('div')
+    let down_title_p_1 = document.createElement('p')
+    let down_title_p_2 = document.createElement('p')
+
+
+    let addres = document.createElement('div')
+    let addres_a = document.createElement('a')
+
+    one_variant.classList.add('one_variant')
+
+    about_us_contener_h1.classList.add('h1')
+    about_us_contener_a_1.classList.add('a')
+    about_us_contener_a_2.classList.add('a')
+
+    for_users_contner_h1.classList.add('h1')
+    for_users_contner_a_1.classList.add('a')
+    for_users_contner_a_2.classList.add('a')
+
+    for_entrepreneurs_contner_h1.classList.add('h1')
+    for_entrepreneurs_contner_a_1.classList.add('a')
+    for_entrepreneurs_contner_a_2.classList.add('a')
+
+    application_box.classList.add('application_box')
+    flex_width_1200px_h1.classList.add('h1')
+    apllication_flex_box.classList.add('apllication_flex_box')
+
+    apple_item.classList.add('w')
+    apple_item_p.classList.add('p')
+
+    google_play_item.classList.add('w')
+    google_play_item_p.classList.add('p')
+
+    uzum_networks_cont.classList.add('uzum_networks_cont')
+    uzum_networks_cont_h1.classList.add('h1')
+    networks.classList.add('networks')
+
+    down.classList.add('down')
+    down_flex.classList.add('down_flex')
+
+    down_title.classList.add('down_title')
+    down_title_p_1.classList.add('p')
+    down_title_p_2.classList.add('p')
+
+
 
     
+}
 
 
