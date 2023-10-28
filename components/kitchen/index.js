2718ddeaa06002f5f2f432f9612c4da15cdca24a
AzamatUzakov <azamatakmal053@gmail.com>
2023-10-28 16:23:17 +0500

    azamat

--- components/kitchen/index.js
+++ components/kitchen/index.js
@@ -18,78 +18,25 @@ let to_main_p = document.querySelector('.to_main')
 let kit_p = document.querySelector('.kit')
 let tovar = document.querySelector('.tovar')
 let bird_img = document.querySelector('.bird_img')
-console.log(cat_p[1]);
 
-function sort() {
-    let sort_cont = document.querySelector('.sort_cont')
+let selected = document.querySelector(".sort")
+let cheap_opt = document.createElement('option')
+let moreExpensive_opt = document.createElement('option')
 
-    let title_sort_cont = document.createElement('div')
-    let title_sort_cont_h1 = document.createElement('h1')
-    let sort_box = document.createElement('div')
-    let sort_box_p = document.createElement('p')
-
-    let selected = document.createElement("select")
-    let cheap_opt = document.createElement('option')
-    let moreExpensive_opt = document.createElement('option')
-    let highRating_opt = document.createElement('option')
-    let orders_opt = document.createElement('option')
-    let added_opt = document.createElement('option')
-
-    let filter_box = document.createElement('div')
-    let filter_img = document.createElement('img')
-    let filter_p = document.createElement('p')
-    let hr = document.createElement('hr')
-
-
-    title_sort_cont.classList.add('title_sort_cont')
-    title_sort_cont_h1.classList.add('h1')
-    sort_box.classList.add('sort_box')
-    sort_box_p.classList.add('p')
-    selected.classList.add('select')
-    filter_box.classList.add('filter_img')
-    filter_p.classList.add('fill')
-    hr.classList.add('hr')
-
-    title_sort_cont_h1.innerHTML = "Кухня"
-    sort_box_p.innerHTML = "Сортировка"
-    cheap_opt.innerHTML = "Подешевле"
-    moreExpensive_opt.innerHTML = "Подароже"
-    highRating_opt.innerHTML = "Высокий рейтинг"
-    orders_opt.innerHTML = "Много заказов"
-    added_opt.innerHTML = "Добавлены не давно"
-    filter_p.innerHTML = "Фильтры"
-
-    filter_img.src = "/public/filter.png"
-
-
-    selected.id = "sort";
-    selected.name = "sort";
-
-    cheap_opt.value = "chaper";
-    cheap_opt.text = "Подешевле";
-
-    moreExpensive_opt.value = "moreExspensive";
-    moreExpensive_opt.text = "Подароже";
-
-    highRating_opt.value = "highRating";
-    highRating_opt.text = "Высокий рейтинг";
-
-    orders_opt.value = "orders";
-    orders_opt.text = "Много заказов";
-
-    added_opt.value = "added";
-    added_opt.text = "Добавлены не давно";
-
-    sort_cont.append(title_sort_cont, hr)
-    title_sort_cont.append(title_sort_cont_h1, sort_box)
-    sort_box.append(sort_box_p, selected, filter_box)
-    selected.append(cheap_opt, moreExpensive_opt, highRating_opt, orders_opt, added_opt)
-    filter_box.append(filter_img, filter_p)
-}
-sort()
+console.log(all_catigories_btn);
 
 
 
+
+
+
+cheap_opt.value = "chaper";
+cheap_opt.text = "aza";
+
+moreExpensive_opt.value = "moreExspensive";
+moreExpensive_opt.text = "Подароже";
+
+selected.append(cheap_opt,moreExpensive_opt)
 
 
 categories.forEach(btn => {
@@ -98,6 +45,7 @@ categories.forEach(btn => {
         btn.classList.add('active')
     }
 });
+
 
 itemBtns.forEach(btn => {
     btn.onclick = () => {
@@ -129,11 +77,13 @@ to_main_p.onclick = () => {
     console.log('click');
 }
 
+let all_products = []
 let kitchen_arr = []
 let white_color_arr = []
 let gray_color_arr = []
 let max_prise = []
 let min_price = []
+let max_rating = []
 
 getData('/goods')
     .then(res => {
@@ -141,10 +91,8 @@ getData('/goods')
             if (item.type === "kitchen") {
                 kitchen_arr.push(item)
             }
-            /*    
-                }) */
+            all_products.push(item)
         }
-
 
 
 
@@ -162,6 +110,8 @@ getData('/goods')
             });
             max_prise.push(item)
             min_price.push(item)
+            max_rating.push(item)
+
         }
 
 
@@ -181,16 +131,101 @@ getData('/goods')
         tovar.onclick = () => {
             reload(kitchen_arr)
         }
+        all_catigories_btn.onclick = () => {
+            reload(all_products)
+            electriks_href.style.display = "none"
+        }
+        // localStorage.setItem("aza", all_products)
+
 
 
         max_prise.sort((a, b) => (+a.price) - (+b.price));
         min_price.sort((a, b) => (-a.price) - (-b.price));
+        max_rating.sort((a, b) => (-a.rating) - (-b.rating));
 
-        console.log(max_prise);
-console.log(min_price);
+        /*      console.log(max_prise);
+             console.log(min_price); */
+        console.log(max_rating);
 
+
+        if (selected === moreExpensive_opt.value) {
+            reload(max_prise)
+            window.onload()
+        }
     }
     )
+
+
+
+function sort() {
+    let sort_cont = document.querySelector('.sort_cont')
+
+    let title_sort_cont = document.createElement('div')
+    let title_sort_cont_h1 = document.createElement('h1')
+    let sort_box = document.createElement('div')
+    let sort_box_p = document.createElement('p')
+
+    let selected = document.createElement("select")
+    let cheap_opt = document.createElement('option')
+    let moreExpensive_opt = document.createElement('option')
+    let highRating_opt = document.createElement('option')
+    let orders_opt = document.createElement('option')
+
+    let filter_box = document.createElement('div')
+    let filter_img = document.createElement('img')
+    let filter_p = document.createElement('p')
+    let hr = document.createElement('hr')
+
+
+    title_sort_cont.classList.add('title_sort_cont')
+    title_sort_cont_h1.classList.add('h1')
+    sort_box.classList.add('sort_box')
+    sort_box_p.classList.add('p')
+    selected.classList.add('select')
+    filter_box.classList.add('filter_img')
+    filter_p.classList.add('fill')
+    hr.classList.add('hr')
+
+    title_sort_cont_h1.innerHTML = "Кухня"
+    sort_box_p.innerHTML = "Сортировка"
+    cheap_opt.innerHTML = "Подешевле"
+    moreExpensive_opt.innerHTML = "Подароже"
+    highRating_opt.innerHTML = "Высокий рейтинг"
+
+    filter_p.innerHTML = "Фильтры"
+
+    filter_img.src = "/public/filter.png"
+
+
+    selected.id = "sort";
+    selected.name = "sort";
+
+    cheap_opt.value = "chaper";
+    cheap_opt.text = "Подешевле";
+
+    moreExpensive_opt.value = "moreExspensive";
+    moreExpensive_opt.text = "Подароже";
+
+    highRating_opt.value = "highRating";
+    highRating_opt.text = "Высокий рейтинг";
+
+
+    sort_cont.append(title_sort_cont, hr)
+    title_sort_cont.append(title_sort_cont_h1, sort_box)
+    sort_box.append(sort_box_p, selected, filter_box)
+    selected.append(cheap_opt, moreExpensive_opt, highRating_opt)
+    filter_box.append(filter_img, filter_p)
+
+
+    if (selected.value === 'Подароже') {
+        reload(max_prise)
+    } else {
+        console.log('hueta');
+    }
+
+    console.log(selected.value === "chaper");
+}
+sort()
 
 function reload(arr) {
     aside_case.innerHTML = ""
